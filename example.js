3125f5b25c918c3ef6a7eec1ca2525fe9928bf9d
Anand Simmy <anandsimmy7@gmail.com>
2021-02-01 08:38:34 +0530

    deleted example.js

--- example.js
+++
@@ -1,9 +0,0 @@
-// Define a global variable 'Module' with a method 'onRuntimeInitialized':
-Module = {
-    onRuntimeInitialized() {
-      // this is our application:
-      console.log(global.cv.getBuildInformation())
-    }
-  }
-  // Load 'opencv.js' assigning the value to the global variable 'cv'
-  cv = require('./js/opencv.js')
