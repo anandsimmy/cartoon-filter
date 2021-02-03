95e7c92834eb262366d39598cd02455aafc9a23d
Anand Simmy <anandsimmy7@gmail.com>
2021-02-03 09:09:36 +0530

    added await spawn

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express= require('express')
-const child_process= require('child_process')
+const spawn= require('await-spawn')
+const { saveImage }= require('./utils/fileUpload.js')
 
 const app= express()
 const PORT= 5000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.get('/filter', (req, res) => {
-    var spawn = child_process.spawn;
-    var process = spawn('python3', ['./image-filter.py'])
+    const spawn = child_process.spawn;
+    const process = spawn('python3', ['./image-filter.py'])
     process.stdout.on('data', (data) => {
-        console.log('data', data)
-        res.send(data);
-      });
+        console.log(data.toString())
+    })
+    return res.sendFile(__dirname+'/filter.jpg')
+})
+
+app.post('/filter', async (req, res) => {
+    let imageFileName= saveImage(req.body.img)
+    console.log('location', imageFileName)
+    await spawn('python3', ['./image-filter.py', imageFileName])
+    return res.sendFile(__dirname+'/filter.jpg')
 })
 
 
