eebe628c4d4212e5e73a71840eaf5cbb9730bf0f
Anand Simmy <anandsimmy7@gmail.com>
2021-02-05 13:28:47 +0530

    increased image size limit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { saveImage }= require('./utils/fileUpload.js')
 const app= express()
 const PORT= 5000
 
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 
 app.get('/', (req, res) => {
     res.send('API is running')
