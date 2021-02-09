724f657f83f24d4c25203b9b318dbc6498a4cc1d
Anand Simmy <anandsimmy7@gmail.com>
2021-02-09 08:58:15 +0530

    added error handling

--- server.js
+++ server.js
@@ -11,23 +11,16 @@ app.use(express.json({limit: '50mb'}))
 
 app.use(cors());
 
-// app.get('/', (req, res) => {
-//     res.send('API is running')
-// })
-
-// app.get('/filter', (req, res) => {
-//     const spawn = child_process.spawn;
-//     const process = spawn('python3', ['./image-filter.py'])
-//     process.stdout.on('data', (data) => {
-//         console.log(data.toString())
-//     })
-//     return res.sendFile(__dirname+'/filter.jpg')
-// })
-
 app.post('/filter', async (req, res) => {
     let imageFileName= saveImage(req.body.img)
-    await spawn('python3', ['./image-filter.py', imageFileName])
-    return res.sendFile(__dirname+'/filter.jpg')
+    try{
+        await spawn('python3', ['./image-filter.py', imageFileName])
+        return res.sendFile(__dirname+'/filter.jpg')
+    }
+    catch(err){
+        console.log('errmessage', err.message)
+        console.error('err', err)
+    }
 })
 
 // This middleware informs the express application to serve our compiled React files
@@ -40,13 +33,6 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 };
-
-// Catch any bad requests
-app.get('*', (req, res) => {
-    res.status(200).json({
-        msg: 'Catch All'
-    });
-});
 
 app.listen(PORT, () => {
     console.log(`Server is listening at port ${PORT}`)
